bca49c6c761fc8df6638fbd13b10a1900bfed474
Diksha <diksha@macbookpro>
2025-12-09 21:08:31 -0800

    working on inserting nodes from data arrays

--- main.cpp
+++ main.cpp
@@ -21,24 +21,21 @@ void coffeePrint(Node *);
 int main(){
 	srand(time(0));
 	Node *coffee = nullptr;
-	Node *temp = new Node;
-	coffee = temp;
-	temp->name = "Diksha";
-	temp->drink = "Mocha";
-	temp->next = nullptr;
-	Node *temp1 = new Node;
-	temp1->name = "Tara";
-	temp1->drink = "Latte";
-	temp1->next = nullptr;
-	coffee->next = temp1;
-	Node *temp2 = new Node;
-	temp2->name = "Shawn";
-	temp2->drink = "Iced Coffee";
-	temp2->next = nullptr;
-	coffee->next->next = temp2;
+	string names[MAX_NAMES] = {"Ava", "Liam", "Olivia", "Noah", "Emma", "Ethan", "Sophia", "Mason", "Isabella", "Logan",
+		"Mia", "Lucas", "Amelia", "Elijah", "Harper", "James", "Charlotte", "Benjamin", "Evelyn", "Henry",
+		"Abigail", "Alexander", "Ella", "Daniel", "Aria", "Matthew", "Scarlett", "Joseph", "Grace", "David",
+		"Chloe", "Samuel", "Lily", "Andrew", "Nora", "Christopher", "Victoria", "William", "Penelope", "Gabriel",
+		"Layla", "Owen", "Riley", "Caleb", "Zoey", "Nathan", "Hannah", "Isaac", "Ellie", "Jonathan",
+		"Paisley", "Julian", "Aubrey", "Christian", "Luna", "Joshua", "Savannah", "Hunter", "Brooklyn", "Aaron",
+		"Stella", "Charles", "Violet", "Dominic", "Claire", "Evan", "Lucy", "Gavin", "Madison", "Wyatt",
+		"Camila", "Jason", "Naomi", "Adrian", "Hazel", "Robert", "Aurora", "Jordan", "Elena", "Parker",
+		"Alice", "Brayden", "Sadie", "Miles", "Ruby", "Cole", "Julia", "Zachary", "Brielle", "Tyler",
+		"Piper", "Leo", "Maya", "Xavier", "Isla", "Asher", "Ivy"};
+	string drinks[MAX_DRINKS] = {"Espresso", "Americano", "Latte", "Cappuccino", "Flat White", "Mocha", "Macchiato", "Cortado", "Cold Brew", "Iced Latte", "Iced Coffee", "Affogato", "Chai Latte", "Matcha Latte", "Frappuccino"};
 
-	string names[100] = {Ava, Liam, Olivia, Noah, Emma, Ethan, Sophia, Mason, Isabella, Logan, Mia, Lucas, Amelia, Elijah, Harper, James, Charlotte, Benjamin, Evelyn, Henry, Abigail, Alexander, Ella, Daniel, Aria, Matthew, Scarlett, Joseph, Grace, David, Chloe, Samuel, Lily, Andrew, Nora, Christopher, Victoria, William, Penelope, Gabriel, Layla, Owen, Riley, Caleb, Zoey, Nathan, Hannah, Isaac, Ellie, Jonathan, Paisley, Julian, Aubrey, Christian, Luna, Joshua, Savannah, Hunter, Brooklyn, Aaron, Stella, Charles, Violet, Dominic, Claire, Evan, Lucy, Gavin, Madison, Wyatt, Camila, Jason, Naomi, Adrian, Hazel, Robert, Aurora, Jordan, Elena, Parker, Alice, Brayden, Sadie, Miles, Ruby, Cole, Julia, Zachary, Brielle, Tyler, Piper, Leo, Maya, Xavier, Isla, Asher, Ivy};
-	string drinks[15] = {Espresso, Americano, Latte, Cappuccino, Flat White, Mocha, Macchiato, Cortado, Cold Brew, Iced Latte, Iced Coffee, Affogato, Chai Latte, Matcha Latte, Frappuccino};
+	insert(coffee, names, drinks);
+	insert(coffee, names, drinks);
+	insert(coffee, names, drinks);
 
 	Node *curr = coffee;
 	coffeePrint(curr);
@@ -56,9 +53,20 @@ int main(){
 }
 
 void insert(Node *n, string[] names, string[] drinks){
-	Node *newNode = new Node;
+	Node *temp = new Node;
 	int index = rand() % MAX_NAMES;	//random number 0-99
-
+	temp->name = names[index];
+	index  = rand() % MAX_DRINKS;	//rand num 0-14
+	temp->drink = drinks[index];
+	temp->next = nullptr;
+	if (!n){
+		n = temp;
+		return;
+	}
+	Node *curr = n;
+	while (curr->next != nullptr)
+		curr = curr.next;
+	curr->next = temp;
 }
 
 void coffeePrint(Node *n){
