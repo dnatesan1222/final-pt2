481cd3c4690cbe33293c1bf323ba11febf1048a5
Diksha <diksha@macbookpro>
2025-12-09 20:58:02 -0800

    working on inserting nodes from data arrays

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@
 #include <fstream>
 using namespace std;
 
+const int MAX_NAMES = 100;
+const int MAX_DRINKS = 15;
+
 struct Node{
 	string name;
 	string drink;
 	Node *next;
 };
 
+void insert(Node *);
 void coffeePrint(Node *);
 
 int main(){
+	srand(time(0));
 	Node *coffee = nullptr;
 	Node *temp = new Node;
 	coffee = temp;
@@ -32,8 +37,8 @@ int main(){
 	temp2->next = nullptr;
 	coffee->next->next = temp2;
 
-	string names[100];
-	string drinks[15];
+	string names[100] = {Ava, Liam, Olivia, Noah, Emma, Ethan, Sophia, Mason, Isabella, Logan, Mia, Lucas, Amelia, Elijah, Harper, James, Charlotte, Benjamin, Evelyn, Henry, Abigail, Alexander, Ella, Daniel, Aria, Matthew, Scarlett, Joseph, Grace, David, Chloe, Samuel, Lily, Andrew, Nora, Christopher, Victoria, William, Penelope, Gabriel, Layla, Owen, Riley, Caleb, Zoey, Nathan, Hannah, Isaac, Ellie, Jonathan, Paisley, Julian, Aubrey, Christian, Luna, Joshua, Savannah, Hunter, Brooklyn, Aaron, Stella, Charles, Violet, Dominic, Claire, Evan, Lucy, Gavin, Madison, Wyatt, Camila, Jason, Naomi, Adrian, Hazel, Robert, Aurora, Jordan, Elena, Parker, Alice, Brayden, Sadie, Miles, Ruby, Cole, Julia, Zachary, Brielle, Tyler, Piper, Leo, Maya, Xavier, Isla, Asher, Ivy};
+	string drinks[15] = {Espresso, Americano, Latte, Cappuccino, Flat White, Mocha, Macchiato, Cortado, Cold Brew, Iced Latte, Iced Coffee, Affogato, Chai Latte, Matcha Latte, Frappuccino};
 
 	Node *curr = coffee;
 	coffeePrint(curr);
@@ -47,6 +52,12 @@ int main(){
 
 
 	}
+
+}
+
+void insert(Node *n, string[] names, string[] drinks){
+	Node *newNode = new Node;
+	int index = rand() % MAX_NAMES;	//random number 0-99
 
 }
 
