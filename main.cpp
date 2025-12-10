1c8f390165cd113e217eb3933fdbb5ac6908022e
Diksha <diksha@macbookpro>
2025-12-09 20:16:25 -0800

    starting work

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// COMSC-210 | Final Pt.2 | Diksha Tara Natesan
+// IDE used: Vim/Terminal
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+
+
+}
+
